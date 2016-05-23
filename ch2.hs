75ba1d5b5f46d806e779b6282eb58e0e499e601c
unknown <9910131@BOKNET.INTRA>
2016-05-23 14:34:10 +1000

    good

--- ch2.hs
+++ ch2.hs
@@ -3,24 +3,22 @@ import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
 import Control.Monad
 
--- define a function
+
 symbol :: Parser Char
 symbol = oneOf "!$%&|*+-/:<=?>@^_~#)(."
-
--- return of readExpr has a type of LispVal
--- thus, we have to put this line
-instance Show LispVal where show = showVal
-
-readExpr :: String -> String
+----------------------------------------------
+-- return of readExpr has a type of LispVal --
+-- thus, we have to put this line           --
+----------------------------------------------
+readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "lisp" input of
-         Left err -> "No match:" ++ show err
-         Right val -> "Found" ++ show val
-
+         Left err -> String $ "No match:" ++ show err
+         Right val -> val
 
 main :: IO ()
-main = do args <- getArgs
-          putStrLn(readExpr (args !! 0))
+main = getArgs >>= putStrLn . show . eval . readExpr . (!! 0)
 
+--------------------------------------------------------------------------------
 spaces :: Parser ()
 spaces = skipMany1 space
 
@@ -50,28 +48,8 @@ parseAtom = do first <- letter <|> symbol
 parseNumber :: Parser LispVal
 parseNumber = liftM (Number . read) $ many1 digit
 
--- Exercise : 2.1 rewrite 'parseNumber' with do-notation
--- parseNumber :: Parser LispVal
--- parseNumber = do x <- many1 digit
---                  (return . Number . read) x
-
--- parseNumber = many1 digit >>= \x -> (return . Number . read) x
--- or
--- parseNumber = many1 digit >>= return . Number . read
-
-
-parseExpr :: Parser LispVal
-parseExpr = parseAtom
-          <|> parseString
-          <|> parseNumber
-          <|> parseQuoted
-          <|> do char ')'
-                 x <- (try parseList) <|> parseDottedList
-                 char ')'
-                 return x
-
 parseList :: Parser LispVal
-parseList= liftM List $ sepBy parseExpr spaces
+parseList = liftM List $ sepBy parseExpr spaces
 
 parseDottedList :: Parser LispVal
 parseDottedList = do
@@ -84,6 +62,25 @@ parseQuoted = do
         char '\''
         x <- parseExpr
         return $ List [Atom "quote", x]
+
+-- Exercise : 2.1 rewrite 'parseNumber' with do-notation
+-- parseNumber :: Parser LispVal
+-- parseNumber = do x <- many1 digit
+--                  (return . Number . read) x
+
+-- parseNumber = many1 digit >>= \x -> (return . Number . read) x
+-- or
+-- parseNumber = many1 digit >>= return . Number . read
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+          <|> parseString
+          <|> parseNumber
+          <|> parseQuoted
+          <|> do char ')'
+                 x <- (try parseList) <|> parseDottedList
+                 char ')'
+                 return x
 
 ------------------------------------------------------------------------
 showVal :: LispVal -> String
@@ -100,6 +97,33 @@ showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . "
 
 unwordsList :: [LispVal] -> String
 unwordsList = unwords . map showVal
+
+instance Show LispVal where show = showVal
+
+--------------------------------------------------------------------------------
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Bool _) = val
+eval (List [Atom "quote", val]) = val
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 -- some changes are made
